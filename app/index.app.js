7ff23f4b6fec245bca3f593bfae55db37c6c4c72
fernandoituarte <fernando.ituarte@oclock.school>
2024-06-20 19:06:48 +0200

    Cors options added

--- app/index.app.js
+++ app/index.app.js
@@ -8,7 +8,12 @@ import router from './routers/index.router.js';
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use(cookieParser());
 app.use(express.json());
 
